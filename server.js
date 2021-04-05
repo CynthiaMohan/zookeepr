500acc30c0b76cb9188ca3f4a84bc3dfc903d439
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-04-05 04:26:28 -0700

    Add Heroku

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+const { animals } = require('./data/animals');
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraitsArray) {
+        if (typeof query.personlityTraits === 'string') {
+            personalityTraitsArray = [query.personlityTraits];
+        } else {
+            personalityTraitsArray = query.personlityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return (filteredResults);
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log('API Server now on port 3001!');
+});
